a27e8144ebab71a62453cdebd69d879aba9fc5dd
hakyung <diveindeep.dev@gmail.com>
2023-05-19 16:59:15 +0900

    Add submodule for contents in private repository

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+const config = require('./contents/config');
+
 module.exports = {
+  siteMetadata: {
+    siteTitle: config.siteTitle,
+    author: config.author,
+    nav: config.nav,
+  },
   plugins: [`gatsby-plugin-styled-components`],
 };
